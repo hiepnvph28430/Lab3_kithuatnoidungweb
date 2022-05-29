62e8f3844428a36b140de91a47ac6c3f927b157a
hiepnvph28430 <mrhiep165@gmail.com>
2022-05-29 15:15:25 +0700

    cap nhat

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ const { resolve } = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        aboutUs: resolve(__dirname, 'aboutUs.html'),
-        products: resolve(__dirname, 'products.html')
-       
-      }
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                aboutUs: resolve(__dirname, 'aboutUs.html'),
+                products: resolve(__dirname, 'products.html')
+
+            }
+        }
     }
-  }
 })
